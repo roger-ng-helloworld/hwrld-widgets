14292e981a4303b68b58d2295aae8639ced1e4c1
Roger Ng <roger.ng@helloworld.com.au>
2016-02-19 14:01:48 +1100

    - activities done

--- js/activities.js
+++ js/activities.js
@@ -13,27 +13,36 @@ export default class Activities extends Component {
 					{this.props.deals.items.map(function(val, ind){			            
 			            return (
 			            	<li key={ind} className={self.props.css['list-item'] + ' row'}>
-			            		<a href={val.cta} className={self.props.css.cta}>CTA</a>          		
-			            		<div className={self.props.css['img-container'] + ' col-sm-3'}>	
-			            			<img src={val.img} alt={val.title} />
+			            		<a href={val.cta} className={self.props.css.cta}></a>          		
+			            		<div className="col-sm-3">
+			            			<div className={self.props.css['img-panel']}>	
+			            				<img src={val.img} alt={val.title} />
+			            			</div>	
 			            		</div>
-			            		<div className="col-sm-6">
+			            		<div className="col-sm-5 col-md-6">
 				            		<div className={self.props.css.title}>{val.title}</div>
 				            		<div>{val.subTitle}</div>
-				            		<div>{val.rating}</div>
+				            		<div>{val.rating} <img className={self.props.css.stars} src={'http://www.helloworld.com.au/l/Images/star-rating-' + Math.round(Number(val.rating)) + '.png'} alt="star rating" />
+				            		</div>
 				            		<div className={self.props.css.blurb}>{val.blurb}</div>        		
 				            	</div>	
-				            	<div className="col-sm-3">
-				            		<div>{val.price.topAggregator}</div>
-				            		<div>{val.price.price}</div>
-				            		<div>{val.price.bottomAggregator}</div>
+				            	<div className={self.props.css['price-container'] + ' col-sm-4 col-md-3'}>
+				            		<div className={self.props.css['arrow-panel']}>
+				            			<i></i>
+				            		</div>
+				            		<div className={self.props.css['price-panel']}>
+					            		<div className={self.props.css['top-aggregator']}>{val.price.topAggregator}</div>
+					            		<div className={self.props.css.price}>${val.price.price}*</div>
+					            		<div className={self.props.css['bottom-aggregator']}>{val.price.bottomAggregator}</div>
+				            		</div>
+				            		
 				            	</div>
 			            	</li>
 			            )
 			       	})}
 				</ul>
 
-				<div>Activities</div>
+				<a href="this.props.general.moreActivities" className={this.props.css['see-more']}>See more Activities</a>
 				
         	</div>
 		);
